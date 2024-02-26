d6448208d6b4b2d275fd4a622780609b48fed374
flitzso <flitzcode@hotmail.com>
2024-02-26 07:46:58 -0300

    Novo sistema security

--- src/main/java/com/hotel81/Hotel81BackendApplication.java
+++ src/main/java/com/hotel81/Hotel81BackendApplication.java
@@ -2,8 +2,9 @@ package com.hotel81;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class Hotel81BackendApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/hotel81/repository/QuartosRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hotel81.repository;
-
-import com.hotel81.entity.Quartos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuartosRepository extends JpaRepository<Quartos, Long> {
-}
